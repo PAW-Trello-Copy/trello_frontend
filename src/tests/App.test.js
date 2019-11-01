bd756c80d2cf1b5cbc739279b0c6f7d191b6731c
Volodymyr.Kovalyk <211646@edu.p.lodz.pl>
2019-11-01 22:16:58 +0100

    test update

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import {Provider} from "react-redux"  //The <Provider /> makes the Redux store available to any nested components that have been wrapped in the connect() function.
+import App from './components/App.js';
+import store from "./store/store"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store ={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
