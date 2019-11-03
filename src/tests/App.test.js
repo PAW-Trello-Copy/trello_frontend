8561b01ff4aa3e7d1678055282086cb3530941e5
Volodymyr.Kovalyk <211646@edu.p.lodz.pl>
2019-11-03 10:50:41 +0100

    easy test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from "react-redux"  //The <Provider /> makes the Redux store available to any nested components that have been wrapped in the connect() function.
-import App from './components/App';
-import store from "./store/store"
+import ReactDOM from 'react-dom'; 
+import TableList from './../components/TableList';
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store ={store}><App /></Provider>, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(<TableList />, div);
 });
