02627a908fc6fb1f2b54bde69612071d47b45c26
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-11-04 21:49:00 +0100

    fetch data from api

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,75 @@
 import React, { Component } from 'react';
 import TableInList from "./TableInList"
-import {connect} from "react-redux"
+import { connect } from "react-redux"
 import MenuTable from "./MenuTable"
 import '../style/App.css';
 
 class App extends Component {
-render(){
-  const {lists} = this.props;
-  return (
-    <div className="App">
-        
-<MenuTable />
-      <div style ={styles.listTableStyle}>        
-        {lists.map(list =><TableInList key={lists.id} title={list.title}/>)}
-        </div>  
-       
-      
-    </div>
-  );
-}
-}
 
+  fetchTables() {
+    fetch('https://paw-trello-backend.herokuapp.com//tables')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          tables: data,
+          isLoading: false
+        }))
+      .catch(error => this.setState({error, isLoading: false }));
+  }
+
+  constructor() {
+    super();
+    this.state = { // TODO: MOVE TO SEPARATE COMPONENT!?
+      tables: [],
+      isLoading: true,
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    this.fetchTables();
+  }
+
+  render() {
+    // const {lists} = this.props;
+    const { isLoading, tables, error } = this.state
+    return (
+      <div className="App">
+        // Display a message if we encounter an error
+        {error ? <p>{error.message}</p> : null}
+        <div style={styles.listTableStyle}>
+          {!isLoading ? (
+            tables.map(table => {
+              const { title, id } = table;
+              return (<TableInList title={table.title} id={table.id} />);
+            })
+          ) : (
+              <h3>Loading tables...</h3>
+            )}
+        </div>
+        {/* <div style={styles.listTableStyle}>
+          {lists.map(list => <TableInList title={list.title} />)}
+        </div> */}
+
+
+      </div>
+    );
+  }
+}
 
 const styles = {
-  listTableStyle:{
-    display:"flex",
-    flexDirection:"row",
-    marginLeft:10
+  listTableStyle: {
+    display: "flex",
+    flexDirection: "row",
+    marginLeft: 10
   }
 };
 
 const mapStateToProps = state => ({
-  lists:state.lists
+  lists: state.lists
 })
 
 
 
 
-export default connect(mapStateToProps) (App);
+export default connect(mapStateToProps)(App);
--- src/components/TableInList.js
+++ src/components/TableInList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const TableInList = ({title,cards}) =>{
+const TableInList = ({title, id}) =>{
     return(
     <div style={styles.container}>
-        <h3>{title}</h3>        
+        <h3>{title}</h3> 
+        <h2> {id}</h2>       
     </div>);
 };
 
